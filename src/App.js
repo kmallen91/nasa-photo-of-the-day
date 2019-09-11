d582c78bcde58b05aea60b3590f2042466cdc136
Kevin Allen <kevin.allen91@gmail.com>
2019-09-11 14:00:11 -0700

    added header with logo

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import Photo from "./photo"
 
 function App() {
   return (
+    <div className="container">
+    <div className="App-header">
+      <img className="App-logo" alt="NASA logo" src="https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png" /> 
+    </div>
+
     <div className="App">
       
       <Photo />
     </div>
-  );
+    </div>
+  )
 }
 
 export default App;
